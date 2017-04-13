8d9a8a32de4a300e47c9aa6c7f1202702165a834
tayyabamukhtar <delicatetabby16@gmail.com>
2017-04-13 20:00:39 +0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,14 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code
-        StudentClass st=new StudentClass();
-        StudentClass st1=new StudentClass();
-        System.out.println(st.username);
-        System.out.println(st.rollnumber);
-        st1.username="ssuet";
-        st1.rollnumber=101;
-        System.out.println(st1.username);
-        System.out.println(st1.rollnumber);
+	// write your code here
+        employee emp=new employee();
+        System.out.println(emp.name);
+        System.out.println(emp.age);
+        System.out.println(emp.designation);
+        System.out.println(emp.salary);
     }
 }
---
+++ employee.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by lab2 on 2017-04-09.
+ */
+public class employee {
+    String name="xyz";
+    int age=16;
+    String designation="java developer";
+    int salary=15000;
+
+}
